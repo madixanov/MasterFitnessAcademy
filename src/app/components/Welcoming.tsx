09e867c8a5ec072c661062a25b6f5d7bd6dcf04a
Нщлги <yoqub.madixanov@outlook.com>
2025-10-30 18:03:28 +0500

    Adding Link component to buttons

--- src/app/components/Welcoming.tsx
+++ src/app/components/Welcoming.tsx
@@ -1,4 +1,5 @@
 import MainContainer from "@/components/MainContainer"
+import Link from "next/link"
 
 export default function WelcomingContainer() {
   return (
@@ -9,10 +10,14 @@ export default function WelcomingContainer() {
           <p className="text-xl lg:text-4xl block">Тренировки и курсы от <br />лучших инструкторов <br />Master <span className="text-[#FF6600]">Fitness</span> Academy</p>
         </article>
         <div className="flex gap-5 justify-start items-center">
-          <button className="text-white cursor-pointer bg-[#FF6600] w-50 py-2 font-semibold text-lg rounded-lg lg:w-60 lg:h-15 transition-all duration-300 hover:bg-white hover:text-[#FF6600]">ЗАПИСАТЬСЯ</button>
-          <button className="bg-black cursor-pointer w-35 py-2 rounded-lg border-3 border-white drop-shadow-[0_0_43px_rgba(255,255,255,0.25)] lg:w-45 lg:h-15 transition-all duration-300 hover:bg-white hover:text-[#FF6600]">
-            <span className="font-semibold text-lg inline-block text-[#FF6600]" style={{ textShadow: '0 0 15px #FF6600, 0 0 55px #FF6600' }}>КУРСЫ</span>
-          </button>
+          <Link href="/catalog">
+            <button className="text-white cursor-pointer bg-[#FF6600] w-50 py-2 font-semibold text-lg rounded-lg lg:w-60 lg:h-15 transition-all duration-300 hover:bg-white hover:text-[#FF6600]">ЗАПИСАТЬСЯ</button>
+          </Link>
+          <Link href="/catalog/courses">
+            <button className="bg-black cursor-pointer w-35 py-2 rounded-lg border-3 border-white drop-shadow-[0_0_43px_rgba(255,255,255,0.25)] lg:w-45 lg:h-15 transition-all duration-300 hover:bg-white hover:text-[#FF6600]">
+              <span className="font-semibold text-lg inline-block text-[#FF6600]" style={{ textShadow: '0 0 15px #FF6600, 0 0 55px #FF6600' }}>КУРСЫ</span>
+            </button>
+          </Link>
         </div>
       </MainContainer>
     </div>
