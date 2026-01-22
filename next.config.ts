62fadbc3318e62b465e9ced54b14263760e04226
Yokub <yoqub.madixanov@outlook.com>
2026-01-22 23:41:00 +0500

    Fixing bugs

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: ["localhost", "www.telsot.uz", "master-fitness.netlify.app", "telsot.uz"], // добавляем все нужные домены
-  },
-  async rewrites() {
-    return [
+    // Безопасный способ разрешить загрузку изображений с внешних доменов
+    remotePatterns: [
       {
-        source: "/api/:path*",                  // все запросы к /api/*
-        destination: "https://www.telsot.uz/:path*", // проксируем на бекенд
+        protocol: "https",
+        hostname: "www.telsot.uz",
+        pathname: "/**",
       },
-    ];
+      {
+        protocol: "https",
+        hostname: "telsot.uz",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "master-fitness.netlify.app",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
