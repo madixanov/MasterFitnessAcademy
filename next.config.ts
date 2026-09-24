35e3f2b72f78a24298481e4b3b535f64b239156b
madixanov <yoqub.madixanov@gmail.com>
2026-09-24 20:18:58 +0500

    Добавление нового бэкенда

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "www.telsot.uz", "master-fitness.netlify.app", "telsot.uz"], // добавляем все нужные домены
+    domains: ["localhost", "www.telsot.uz", "master-fitness.netlify.app", "telsot.uz", "185.183.242.15"], // добавляем все нужные домены
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",                  // все запросы к /api/*
-        destination: "https://www.telsot.uz/:path*", // проксируем на бекенд
+        destination: "https://185.183.242.15:path*", // проксируем на бекенд
       },
     ];
   },
